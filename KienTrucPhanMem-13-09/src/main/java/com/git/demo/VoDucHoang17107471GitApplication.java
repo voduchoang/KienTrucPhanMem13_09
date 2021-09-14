535d38e0c94abe9ee460998c3df366c7e85c5afb
vo hoang <voduchoang089@gmail.com>
2021-09-14 23:30:36 +0700

    Push code Kien Truc

---
+++ KienTrucPhanMem-13-09/src/main/java/com/git/demo/VoDucHoang17107471GitApplication.java
@@ -0,0 +1,13 @@
+package com.git.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoDucHoang17107471GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoDucHoang17107471GitApplication.class, args);
+	}
+
+}
---
+++ KienTrucPhanMem-13-09/src/test/java/com/git/demo/VoDucHoang17107471GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoDucHoang17107471GitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
